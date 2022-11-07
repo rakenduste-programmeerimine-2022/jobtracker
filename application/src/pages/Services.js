c0f8f275f70cf7f5373bf531a90df89797de88b9
knyrr <rynk@tlu.ee>
2022-11-07 18:53:03 +0200

    Andmeskeemide kirjeldus (FE,  BE). Closes #30

--- application/src/pages/Services.js
+++ application/src/pages/Services.js
@@ -1,5 +1,28 @@
 import React from "react"
 
+/*
+Kasutajaliideses:
+- kasutaja id
+- kood
+- kirjeldus
+- ühiku nimi
+- ühiku hind
+- maksumäär (0, 9, 20)
+
+Serveri poolel:
+const serviceSchema = new Schema(
+  {
+    userId: { type: String, required: true},
+    code: { type: String, required: true, unique: true }, 
+    description: { type: String, required: true },
+    unit: { type: String, required: true },
+    price: { type: Decimal128, required: true },
+    tax: { type: Decimal128, enum: ["0.0", "9.0", 20.0"], default: "20.0" },
+  },
+  { timestamps: true }
+)
+*/
+
 const Services = () => {
   return <div>Services</div>
 }
