e9b6910aa9d76fa2c3ca7493e6ee2b790e91f5f8
knyrr <rynk@tlu.ee>
2022-11-25 13:47:37 +0200

    Server toimib

--- Server/Server.js
+++ Server/Server.js
@@ -1,46 +1,48 @@
-const express = require('express')
-const cors = require('cors');
-const cookieSession = require('cookie-session');
-const dotenv = require('dotenv')
+const express = require("express")
+const cors = require("cors")
+const cookieSession = require("cookie-session")
+const dotenv = require("dotenv")
 
-
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
 let corsOptions = {
-    origin: 'http://localhost:3000'
+  origin: "http://localhost:3000",
 }
 
 app.use(cors(corsOptions))
 
 app.use(express.json())
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(
-    cookieSession({
-        name: "JobTracker-session",
-        secret: process.env.SECRET_KEY,
-        maxAge: 2 * 60 * 60 * 1000,
-        httpOnly: true
-    })
+  cookieSession({
+    name: "JobTracker-session",
+    secret: process.env.SECRET_KEY,
+    maxAge: 2 * 60 * 60 * 1000,
+    httpOnly: true,
+  })
 )
 
-require('./Routes/auth.routes')(app);
+require("./Routes/auth.routes")(app)
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on port ${PORT}.`)
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}.`)
 })
 
-const db = require("./Models");
+const db = require("./Models")
 
-console.log(db)
+//console.log(db)
 
-db.mongoose.connect(process.env.DB).then(() => {
+db.mongoose
+  .connect(process.env.DB)
+  .then(() => {
     console.log("MongoDB ühendus olemas")
-}).catch(err => {
-    console.error("Connection error", err);
-    process.exit();
-})
+  })
+  .catch((err) => {
+    console.error("Connection error", err)
+    process.exit()
+  })
